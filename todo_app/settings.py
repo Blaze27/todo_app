bb5ea2ff432d0cb118722bc8fd3400bc1d3a9a48
Rahul Sharma <rahul.sharma@innovaccer.com>
2024-08-24 02:27:04 +0530

    Create model Task

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'test',
     }
 }
 
