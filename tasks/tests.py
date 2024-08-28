e9dc058dab49f21364f1dff0065f6367f4d33b80
Rahul Sharma <rahul.sharma@innovaccer.com>
2024-08-29 00:59:45 +0530

    Update tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,138 +1,59 @@
-# import mongomock
-# import json
-# from django.test import TestCase
-# from rest_framework.test import APIClient
-# from django.contrib.auth.models import User
-# from unittest.mock import patch, MagicMock
-#
-#
-#
-# class TaskViewSetTest(TestCase):
-#     def setUp(self):
-#         self.patcher = patch('tasks.mongodb.MongoClient', return_type=mongomock.MongoClient())
-#         self.mocked_client = self.patcher.start()
-#         self.client = APIClient()
-#         self.user = User.objects.create_user(
-#             username='test-user', password='testpassword', email='testuser@email.com'
-#         )
-#
-#         self.client.force_authenticate(user=self.user)
-#
-#         self.db = self.mocked_client['test_db']
-#         self.collection = self.db['tasks']
-#
-#         self.collection.insert_one = MagicMock()
-#         self.collection.find_one = MagicMock(return_value={
-#             'title': 'test-task',
-#             'description': 'test description',
-#             'creation_date': '2024-08-25',
-#             'due_date': '2024-08-30',
-#             'status': 'pending'
-#         })
-#
-#     def tearDown(self):
-#         self.patcher.stop()
-#
-#     def test_create_task(self):
-#         data = {
-#             'title': 'test-task',
-#             'description': 'test description',
-#             'creation_date': '2024-08-25',
-#             'due_date': '2024-08-30',
-#             'status': 'pending'
-#         }
-#
-#         response = self.client.post(
-#             '/api/tasks/', data=json.dumps(data), content_type='application/json'
-#         )
-#         print(response)
-#         self.assertEqual(response.status_code, 201)
-#         self.assertTrue('_id' in response.data)
-#
-#         task = self.collection.find_one(
-#             {'user_id': 1},
-#             {'title': 'test-task'}
-#         )
-#         self.assertIsNotNone(task)
-#         # import pdb; pdb.set_trace()
-#         # self.assertEqual(task['description'], 'test description')
-#
-#     def test_get_tasks(self):
-#         task_id = self.collection.insert_one({
-#             'title': 'test-task',
-#             'description': 'test description',
-#             'creation_date': '2024-08-25',
-#             'due_date': '2024-08-30',
-#             'status': 'pending',
-#             'user_id': self.user.id
-#         }).inserted_id
-#
-#         import pdb; pdb.set_trace()
-#
-#         response = self.client.get(f'/api/tasks/{task_id}')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(response.data['results']), 1)
-#         print(response.data)
-#         self.assertEqual(response.data[0]['title'], 'test-task')
-    #
-    # def test_update_task(self):
-    #     task_id = self.collection.insert_one({
-    #         'title': 'test-task',
-    #         'description': 'test description',
-    #         'creation_date': '2024-08-25',
-    #         'due_date': '2024-08-30',
-    #         'status': 'pending',
-    #         'user_id': self.user.id
-    #     }).inserted_id
-    #
-    #     update_data = {
-    #         'title': 'updated task',
-    #         'description': 'updated description',
-    #         'due_date': '2024-08-30',
-    #         'status': 'completed'
-    #     }
-    #     response = self.client.put(
-    #         f'/api/tasks/{task_id}', data=json.dumps(update_data), content_type='application.json'
-    #     )
-    #     self.assertEqual(update_data['title'], 'updated task')
-    #     self.assertEqual(update_data['description'], 'updated description')
-    #
-    #
-    # def test_delete_task(self):
-    #     task_id = self.collection.insert_one({
-    #         'title': 'test-task',
-    #         'description': 'test description',
-    #         'creation_date': '2024-08-25',
-    #         'due_date': '2024-08-30',
-    #         'status': 'pending',
-    #         'user_id': self.user.id
-    #     }).inserted_id
-    #
-    #     response = self.client.delete(
-    #         f'/api/tasks/', params={'task_id': 1}
-    #     )
-    #     self.assertEqual(response.status_code, 204)
-    #     task = self.collection.find_one(
-    #         {'_id': task_id}
-    #     )
-    #     self.assertIsNone(task)
-    #
-    #
-    # def test_pagination(self):
-    #     for i in range(10):
-    #         self.collection.insert_one(
-    #             {
-    #                 'title': f'test-task-{i}',
-    #                 'description': f'test description {i}',
-    #                 'creation_date': '2024-08-25',
-    #                 'due_date': '2024-08-30',
-    #                 'status': 'pending',
-    #                 'user_id': self.user.id
-    #             }
-    #         )
-    #
-    #     response = self.client.get(
-    #         '/api/tasks/?page=1&page_size=3'
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(len(response.data), 4)
+from django.test import TestCase
+from django.utils import timezone
+from mongodb import MongoDBClient
+from mongo_operations import (
+        find_one,
+        insert_item,
+        update_item,
+        delete_one
+)
+
+
+class TaskAPITest(TestCase):
+
+    def setUp(self):
+        # Insert a test task
+        self.user = 1
+        self.test_task = {
+            'title': 'Test Task',
+            'description': 'Just a test',
+            'creation_date': timezone.now(),
+            'due_date': timezone.now(),
+            'status': 'Pending',
+            'user_id': self.user
+        }
+        self.test_task_id = insert_item(data=self.test_task, collection_name='test').inserted_id
+        self.client = MongoDBClient.get_database()
+        self.collection = self.client['test']
+
+    def tearDown(self):
+        self.collection.drop()
+
+    def test_task_creation(self):
+        task_count = self.collection.count_documents({})
+        self.assertEqual(task_count, 1)
+
+    def test_task_retrieval(self):
+        task = find_one(self.user, self.test_task_id, 'test')
+        self.assertIsNotNone(task)
+        self.assertEqual(task['title'], 'Test Task')
+
+    def test_task_update(self):
+        update_data = {
+            'title': 'new title',
+            'description': 'new description',
+            'due_date': timezone.now(),
+            'status': 'completed',
+            'user_id': self.user
+        }
+        update_item(
+            self.user,
+            self.test_task_id,
+            update_data, 'test')
+        task = find_one(self.user, self.test_task_id, 'test')
+        self.assertEqual(task['status'], 'Completed')
+
+    def test_task_deletion(self):
+        delete_one(self.user, self.test_task_id, 'test')
+        task_count = self.collection.count_documents({})
+        self.assertEqual(task_count, 0)
